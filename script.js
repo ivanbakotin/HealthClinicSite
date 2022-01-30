15f30ebf294cd26a2bff6fe7e331daff2373803f
ivan <ivan.bakotin1999@gmail.com>
2022-01-30 21:41:30 +0100

    images

---
+++ script.js
@@ -0,0 +1,23 @@
+const slides = document.getElementsByClassName("mySlides");
+
+let counter =  0;
+
+slides[0].style.display = "block";
+
+setInterval(() => {
+
+    for (i = 0; i < slides.length; i++) {
+        if (counter === i) {
+            slides[i].style.display = "block";
+        } else {
+            slides[i].style.display = "none";
+        }
+    }
+
+    if (counter > 1) {
+        counter  = 0;
+    } else {
+        counter++;
+    }
+
+}, 3000)
